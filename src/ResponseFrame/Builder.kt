6356eb852165bac8cb81e6cb4d2973c064497137
xxSk8xx <carsonaurum@icloud.com>
2018-03-16 16:07:42 -0500

    Second guessing the value of kotlin the the response system...

---
+++ src/ResponseFrame/Builder.kt
@@ -0,0 +1,9 @@
+package ResponseFrame
+
+import net.dv8tion.jda.core.entities.MessageEmbed
+
+object Builder {
+    fun build (response : Response) : MessageEmbed {
+        
+    }
+}
--- src/ResponseFrame/Response.kt
+++ src/ResponseFrame/Response.kt
@@ -1,3 +1,20 @@
 package ResponseFrame
 
-class Response(val type : ResponseType)
+class Response constructor (val type : ResponseType, val responseNumber : Number, val args : Array<String>) {
+
+    var footer : String?
+    var image : String?
+    var title : String?
+    var description : String?
+    var fields : Array<Field>?
+
+    init {
+        footer = null
+        image = null
+        title = null
+        description = null
+        fields = null
+    }
+}
+
+class Field constructor(val title : String, val text : String, val inline : Boolean)
